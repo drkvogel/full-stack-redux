dfc7ace95d3095f5c92f37695e7d34557c4d4fca
Chris Bird <chrisjbird@gmail.com>
2017-02-27 14:25:58 +0700

    more

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 describe('immutability', () => {
     describe('a number', () => {
@@ -14,28 +14,33 @@ describe('immutability', () => {
         })
     })
 
-    describe('a List', () => {
+    describe('a tree', () => {
         function addMovie(currentState, movie) {
-            return currentState.push(movie);
-            // return currentState.set(
-            //     'movies',
-            //     currentState.get('movies').push(movie)
-            // );
+            return currentState.set(
+                'movies',
+                currentState.get('movies').push(movie)
+            );
         };
 
         it('is immutable', () => {
-            let state = List.of('Trainspotting', '28 Days Later');
+            let state = Map({
+                movies: List.of('Trainspotting', '28 Days Later')
+            });
             let nextState = addMovie(state, 'Sunshine');
 
-            expect(nextState).to.equal(List.of(
-                'Trainspotting',
-                '28 Days Later',
-                'Sunshine'
-            ));
+            expect(nextState).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting',
+                    '28 Days Later',
+                    'Sunshine'
+                )
+            }));
 
-            expect(state).to.equal(List.of(
-                'Trainspotting', '28 Days Later'
-            ));
+            expect(state).to.equal(Map({
+                movies: List.of(
+                    'Trainspotting', '28 Days Later'
+                )
+            }));
         });
     });
 });
