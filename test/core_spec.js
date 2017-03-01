7a88bdb983be03f0788c2d634becbb90398224f3
Chris Bird <chrisjbird@gmail.com>
2017-03-02 05:18:51 +0700

    Starting Voting

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,6 +1,6 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
     describe('setEntries', () => {
@@ -21,4 +21,19 @@ describe('application logic', () => {
             }));
         });
     });
+
+    describe('next', () => {
+        it('takes the next two entries under vote', () => {
+            const state = Map({
+                entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Trainspotting', '28 Days Later')
+                }),
+                entries: List.of('Sunshine')
+            }))
+        })
+    })
 })
