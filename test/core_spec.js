5b4cd481a3275ff0d5b03118c179436782e9e51d
Chris Bird <chrisjbird@gmail.com>
2017-03-02 06:12:28 +0700

    End of Vote test failing

--- test/core_spec.js
+++ test/core_spec.js
@@ -76,6 +76,21 @@ describe('application logic', () => {
             }))
         })
 
+        it('marks winner when just one entry left', () => {
+            const state = Map({
+                pair: List.of('Trainspotting', '28 Days Later'),
+                tally: Map({
+                    'Trainspotting': 4,
+                    '28 Days Later': 2
+                }),
+                entries: List()
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                winner: 'Trainspotting'
+            }));
+        });
+
     });
 
     describe('vote', () => {
